0aa06cbf486af347552afe239d9811176ff31c27
sp0f <adam.lyjak@gmail.com>
2017-04-08 20:08:45 +0200

    message as JSON + refactoring

--- src/beacon.ino
+++ src/beacon.ino
@@ -1,9 +1,12 @@
 // HC-SR04: VCC - 5V, trigger - 3V
 // WeMos D1 (ESP8266): D1 - trigger, D2 - echo, D0 and RST connected for deep sleep mode
 // MQTT using https://github.com/Imroy/pubsubclient
+// JSON using: https://github.com/bblanchon/ArduinoJson
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+
 
 #define trigPin 5 //D1
 #define echoPin 4 //D2
@@ -38,27 +41,42 @@ void setup() {
   Serial.println("");
   Serial.print("[*] WiFi connected [");
   Serial.print(WiFi.localIP());
-  Serial.print("]");
+  Serial.println("]");
 }
 
 
 void loop() {
   int distance;
+  int deviceId;
+  int state;
+  
+  StaticJsonBuffer<256> jsonBuffer;
+   
+   deviceId = ESP.getChipId();
 
   // distance = 0 measn measurement error
   do {
     distance = measureDistance();
   } while (distance == 0);
+
+  if (distance < UNOCCUPIED_DISTANCE) {
+    state = 10;
+  } else {
+    state = 0;
+  }
+
+  JsonObject& jsonMessage = buildJson(jsonBuffer, distance, deviceId, VERSION, state);
+  String message;
+  jsonMessage.printTo(message);
   
   Serial.print("Distance: ");
   Serial.println(distance);
   // send to mqtt
-  
-  if (distance < UNOCCUPIED_DISTANCE) {
-    mqttPublish(1);
+  mqttPublish(message, deviceId);
+
+  // nobody is that quick in toilet ...
+  if (state)  {
     delay(sleepTime * 1000);
-  } else {
-    mqttPublish(0);
   }
 }
 
@@ -67,16 +85,23 @@ void mqttCallback(const MQTT::Publish& pub) {
   // handle message arrived
 }
 
-void mqttPublish(int stat) {
+void mqttPublish(String message, int intDeviceId) {
   if (WiFi.status() == WL_CONNECTED) {
-
+    String deviceId = String(intDeviceId);
+    
     // if not connected -> connect and publish
     if (!client.connected()) {
       Serial.println("Connecting to MQTT server");
-      if (client.connect(MQTT::Connect(String(ESP.getChipId())).set_auth(mqttUser, mqttPassword))) {
+      if (client.connect(MQTT::Connect(deviceId)
+        .set_auth(mqttUser, mqttPassword)
+        .set_keepalive(60)))
+      {
         Serial.println("Connected to MQTT server");
         client.set_callback(mqttCallback);
-        client.publish("toilet/"+String(OFFICE)+"/"+String(ESP.getChipId()),String(stat));
+        client.publish(MQTT::Publish("toilet/"+String(OFFICE)+"/"+deviceId,message)
+          .set_qos(1)
+          .set_retain()
+        );
       } else {
         Serial.println("Could not connect to MQTT server");
       }
@@ -85,15 +110,30 @@ void mqttPublish(int stat) {
     else {
         Serial.println("Connected to MQTT server");
         client.set_callback(mqttCallback);
-        client.publish("toilet/"+String(OFFICE)+"/"+String(ESP.getChipId()),String(stat));
+        client.publish(MQTT::Publish("toilet/"+String(OFFICE)+"/"+deviceId,message)
+          .set_qos(1)
+          .set_retain()
+        );
     }
 
     // console debug
-    String message = "toilet/"+String(OFFICE)+"/"+String(ESP.getChipId())+"/"+String(stat);
     Serial.println(message);
   }
 }
 
+JsonObject& buildJson(JsonBuffer& jsonBuffer, int distance, int deviceId, int ver, int state)  {
+  JsonObject& root = jsonBuffer.createObject();
+
+  root["deviceId"] = deviceId;
+  root["status"] = state;
+  root["version"] = ver;
+  
+  JsonArray& measurements = root.createNestedArray("measurements");
+  JsonObject& nested = measurements.createNestedObject();
+  nested["distance"] = distance;
+
+  return root;
+}
 int measureDistance()  {
   /* Measure the distance using HC-SR04 sensor 
      Retur: distance (int) in cm */
